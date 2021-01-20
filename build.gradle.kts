9df775efc77d8de98a37dd0e1092960c20ad80d2
Ángel Gómez <raxden.dev@gmail.com>
2021-01-20 11:22:26 +0100

    increase version plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,13 +58,13 @@ pluginBundle {
             // id is captured from java-gradle-plugin configuration
             displayName = "Gradle Publish plugin"
             tags = listOf("android")
-            version = "0.20"
+            version = "0.21"
         }
         "versioningPlugin" {
             // id is captured from java-gradle-plugin configuration
             displayName = "Gradle Versioning plugin"
             tags = listOf("android")
-            version = "0.20"
+            version = "0.21"
         }
     }
 
@@ -84,7 +84,7 @@ pluginBundle {
     mavenCoordinates {
         groupId = "com.raxdenstudios"
         artifactId = "android-plugins"
-        version = "0.20"
+        version = "0.21"
     }
 }
 
